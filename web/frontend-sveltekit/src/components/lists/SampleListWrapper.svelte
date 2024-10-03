2a256b1f84347ad9167bc5c40526f71b337a0097
Julian Rakuschek <julian.rakuschek@tugraz.at>
2024-10-03 16:15:56 +0200

    Migrated charts from svelte to sveltekit

---
+++ web/frontend-sveltekit/src/components/lists/SampleListWrapper.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import {ApiRoutes} from "@lib/api/ApiRoutes";
+    import {type SamplesSettingsType, SortMode} from "@lib/types";
+    import {useQueryFetch} from "@lib/api/ApiQueries";
+    import SampleList from "@components/lists/SampleList.svelte";
+
+    export let machineId: string;
+
+    export let settings: SamplesSettingsType;
+
+    export let selectModeActive: boolean;
+
+    const sort_samples = (samples: string[], anomaly_ratios: [string, number][]) => {
+        let sample_sorted = samples.sort();
+        if (settings.sort === SortMode.Score && anomaly_ratios.length == samples.length && samples) {
+            sample_sorted = anomaly_ratios.map(s => s[0])
+        }
+        return sample_sorted
+    }
+
+
+    const sampleListQuery = useQueryFetch(ApiRoutes.getMachineSamples, {params: {machineId}})
+    const normalsQuery = useQueryFetch(ApiRoutes.getNormals, {params: {machineId}})
+    const anomalyRatiosQuery = useQueryFetch(ApiRoutes.getAnomalyRatios, {params: {machineId}})
+    let anomaly_ratios: [string, number][] = []
+    let fetching_anomalies = true;
+
+    anomalyRatiosQuery.subscribe((value) => {
+        if (value.isSuccess) {
+            anomaly_ratios = value.data
+            fetching_anomalies = false;
+        }
+    })
+
+</script>
+
+{#if $sampleListQuery.isPending || $normalsQuery.isPending}
+    <p>Loading ...</p>
+{/if}
+
+{#if fetching_anomalies}
+    <div class="fixed bottom-10 left-10 text-center px-4 py-2 bg-indigo-700 text-white rounded-md">Fetching anomaly scores ...</div>
+{/if}
+
+{#if $sampleListQuery.isSuccess && $normalsQuery.isSuccess}
+    <div class="flex flex-row flex-wrap gap-6 py-4 justify-center">
+        {#if !settings.split}
+            <SampleList samples={sort_samples($sampleListQuery.data, anomaly_ratios)} machineId={machineId} normals={$normalsQuery.data}
+                        anomaly_ratios={anomaly_ratios} selectModeActive={selectModeActive} bind:fetching_anomalies/>
+        {:else}
+            <div class="flex flex-row gap-20">
+                <div class="flex flex-col gap-4">
+                    <SampleList samples={sort_samples($sampleListQuery.data, anomaly_ratios).filter(s => s.startsWith("normal"))}
+                                machineId={machineId} normals={$normalsQuery.data} anomaly_ratios={anomaly_ratios}
+                                selectModeActive={selectModeActive} bind:fetching_anomalies/>
+                </div>
+                <div class="flex flex-col gap-4">
+                    <SampleList samples={sort_samples($sampleListQuery.data, anomaly_ratios).filter(s => s.startsWith("abnormal"))}
+                                machineId={machineId} normals={$normalsQuery.data} anomaly_ratios={anomaly_ratios}
+                                selectModeActive={selectModeActive} bind:fetching_anomalies/>
+                </div>
+            </div>
+        {/if}
+    </div>
+{/if}
