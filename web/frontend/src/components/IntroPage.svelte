2be0ade8f1fffb4e30b30e65f5da91202e4e5935
Julian Rakuschek <julian.rakuschek@tugraz.at>
2024-10-20 12:00:31 +0200

    added normal selection to intro page

--- web/frontend/src/components/IntroPage.svelte
+++ web/frontend/src/components/IntroPage.svelte
@@ -30,21 +30,27 @@
         however, the signal on the right-hand side contains anomalies.
         This results in several patterns in the projection, especially in the outer rings.
     </p>
-    <div class="flex flex-row my-5">
+    <br/>
+    <div class="flex flex-row">
         <img src="/intro/images/binder-1.png" class="w-1/2"/>
         <img src="/intro/images/binder-2.png" class="w-1/2"/>
     </div>
+    <br/>
     <p>
         This time delay embedding is at the heart of Vibrana: It offers a visual indicator if the signal is behaving in
         an unexpected way.
     </p>
-    <p class="mb-5">The Vibrana tool offers the following features:</p>
+    <br/>
+    <p>The Vibrana tool offers the following features:</p>
     <ul class="list-disc list-inside ml-5">
         <li>Exploration of vibration signals through time-delay embeddings.</li>
         <li>Annotating windows in the signal.</li>
         <li>Similarity search with the assigned labels to quickly classify signals (anomalous vs normal).</li>
     </ul>
-    <p class="text-2xl font-semibold mt-10 mb-1">How to analyze data with Vibrana</p>
+    <br/>
+    <br/>
+    <p class="text-2xl font-semibold">How to analyze data with Vibrana</p>
+    <br/>
     <p>
         The first step is to upload the signals.
         We assume that our target group wants to analyze vibrations caused by machines, which is why groups of signals
@@ -53,16 +59,19 @@
             href="https://dewesoft.com/de" class="text-indigo-500">DEWESoft</a> products.
     </p>
     {#if ro}
-        <div class="my-5 bg-red-100 text-red-500 rounded-lg p-3"><b>Note:</b>
+        <br/>
+        <div class="bg-red-100 text-red-500 rounded-lg p-3"><b>Note:</b>
             This demo is in read-only mode, therefore the upload feature is disabled.
         </div>
+        <br/>
     {/if}
     <p>
         During upload, the signal is split into smaller chunks to be more manageable by the visualization.
         After the upload is finished, the user may start exploring the chunks of the signal through the three-charts
         view:
     </p>
-    <div class="flex flex-col justify-center items-center relative my-5">
+    <br/>
+    <div class="flex flex-col justify-center items-center relative">
         <img src="/intro/images/vibrana-overview.png"/>
         <div class="absolute top-0 right-0 bg-[#304ffe] text-white rounded-lg p-1 text-sm px-3 text-center">
             <span class="bg-white text-indigo-700 rounded-full px-2 mr-3">1</span> Select a subset
@@ -74,63 +83,88 @@
             <span class="bg-white text-indigo-700 rounded-full px-2 mr-3">3</span> Explore 2D projection
         </div>
     </div>
+    <br/>
     <p>
         First, the color in all three charts corresponds to the radius in the point cloud.
         Therefore, red means the point is far outside while blue points are located near the center.
         Note that this visual mapping is designed to guide users towards points of interest within the signal,
         therefore chart 1 is used in order to zoom in, while chart 2 then presents the selected subset.
     </p>
-    <div class="my-5 bg-indigo-50 text-indigo-500 rounded-lg p-3"><b>Hint:</b>
+    <br/>
+    <div class="bg-indigo-50 text-indigo-500 rounded-lg p-3"><b>Hint:</b>
         Many interesting anomalies tend to accumulate towards the outer rings of the projection, therefore it is
         advisable to examine those regions first.
     </div>
+    <br/>
     <p>
         In order to validate an anomaly, the path tracing tool can be used.
         When the time series is projected into the 2D plane, the patterns tend to form traces.
         It can be observed, that anomalies lead to well structured rings while noise results in chaotic paths.
         The following two tabs show both cases where the path tool is used.
     </p>
-    <div class="flex flex-row w-full justify-center gap-5 mt-5">
-        <button on:click={() => tab_paths = "no_anomaly"}
-                class={`${tab_paths === "no_anomaly" ? "border-indigo-500" : "border-transparent"} border-2 rounded-md bg-indigo-50 px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100`}>
-            Normal Signal
-        </button>
-        <button on:click={() => tab_paths = "anomaly"}
-                class={`${tab_paths === "anomaly" ? "border-indigo-500" : "border-transparent"} border-2 rounded-md bg-indigo-50 px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100`}>
-            Anomalous Signal
-        </button>
-    </div>
-    {#if tab_paths === "no_anomaly"}
-        <div class="w-full flex justify-center">
-            <video src="/intro/videos/paths-no-anomaly.mp4" autoplay loop muted></video>
+    <br/>
+    <StyledDisclosure header_text="Video: Inspecting the time series paths.">
+        <div class="flex flex-row w-full justify-center gap-5">
+            <button on:click={() => tab_paths = "no_anomaly"}
+                    class={`${tab_paths === "no_anomaly" ? "border-indigo-500" : "border-transparent"} border-2 rounded-md bg-indigo-50 px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100`}>
+                Normal Signal
+            </button>
+            <button on:click={() => tab_paths = "anomaly"}
+                    class={`${tab_paths === "anomaly" ? "border-indigo-500" : "border-transparent"} border-2 rounded-md bg-indigo-50 px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100`}>
+                Anomalous Signal
+            </button>
         </div>
-    {/if}
-    {#if tab_paths === "anomaly"}
-        <div class="w-full flex justify-center">
-            <video src="/intro/videos/paths-anomaly.mp4" autoplay loop muted></video>
-        </div>
-    {/if}
-    <div class="my-5 bg-red-100 text-red-500 rounded-lg p-3"><b>Note:</b>
+        {#if tab_paths === "no_anomaly"}
+            <div class="w-full flex justify-center">
+                <video src="/intro/videos/paths-no-anomaly.mp4" autoplay loop muted></video>
+            </div>
+        {/if}
+        {#if tab_paths === "anomaly"}
+            <div class="w-full flex justify-center">
+                <video src="/intro/videos/paths-anomaly.mp4" autoplay loop muted></video>
+            </div>
+        {/if}
+    </StyledDisclosure>
+    <br/>
+    <div class="bg-red-100 text-red-500 rounded-lg p-3"><b>Note:</b>
         This assumes that the background of the signal is noise while anomalies exhibit strong frequency patterns.
         Other datasets may result in different point clouds, but the path tracing tool is still a valid exploration
         tool.
     </div>
-
+    <br/>
     <p>
         As soon as valid anomalies have been discovered, they can be saved as labels using chart 2.
         The labels are used in the next step to classify the signals (normal vs anomalous) using similarity search.
-        That is, the saved anomaly is slided across all signals and in each timestep, the similarity is computed using the euclidean distance.
-        Therefore, a high similarity of the signal to the label or a low distance from the euclidean distance (beware of the duality) indicates that the pattern occurred again.
+        That is, the saved anomaly is slided across all signals and in each timestep, the similarity is computed using
+        the euclidean distance.
+        Therefore, a high similarity of the signal to the label or a low distance from the euclidean distance (beware of
+        the duality) indicates that the pattern occurred again.
     </p>
-    <p class="mt-1">
+    <br/>
+    <p>
         However, for this to be interpretable, we need a notion of what a normal signal looks like.
         This is achieved by selecting a signal without anomalies in the overview (see the following video).
         As soon as a normal sample and labels are available, the anomaly score can be computed for each signal.
     </p>
-
-    <StyledDisclosure header_text="Video: Selecting Normals" >
-        Hi mum
+    <br/>
+    <StyledDisclosure header_text="Video: Selecting Normals">
+        <div class="w-full flex justify-center">
+            <video src="/intro/videos/normal-selection.mp4" autoplay loop muted></video>
+        </div>
     </StyledDisclosure>
+    <br/>
+    <p>
+        The computed distances can be further inspected by choosing a signal and changing the color mode accordingly.
+        This process of selecting labels, selecting normals and computing distances can be repeated until a satisfactory
+        result is achieved.
+    </p>
+    <br/>
+    <div class="bg-red-100 text-red-500 rounded-lg p-3"><b>Note:</b>
+        The classification does not work for every dataset.
+        If the patterns in the normal / anomalous classes are too similar, the classification fails.
+        This can be determined by using the matrix profile (see paper).
+    </div>
+
 
     <div class="w-full h-[500px]"></div>
 </div>
