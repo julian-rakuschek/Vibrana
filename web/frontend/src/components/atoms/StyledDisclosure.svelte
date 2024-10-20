6161dbac55449ac3f6aaeb918d2900725a7c2a9e
Julian Rakuschek <julian.rakuschek@tugraz.at>
2024-10-20 11:13:59 +0200

    styled disclosure

---
+++ web/frontend/src/components/atoms/StyledDisclosure.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+    import {Disclosure, DisclosureButton, DisclosurePanel} from "@rgossiaux/svelte-headlessui";
+    import {Icon, ChevronUp} from "svelte-hero-icons";
+
+    export let header_text: string;
+</script>
+
+<Disclosure class="w-full" let:open>
+    <DisclosureButton class={`bg-indigo-700 text-white ${open ? "rounded-t-lg" : "rounded-lg"} px-4 py-1 w-full flex flex-row justify-between items-center`}>
+        {header_text}
+        <Icon src="{ChevronUp}" solid class="w-7 h-7 text-white transition" style={open ? "transform: rotate(180deg);" : ""}/>
+    </DisclosureButton>
+
+    <DisclosurePanel class="gap-4 flex flex-col border-indigo-700 border-2 p-4 rounded-b-lg ">
+         <slot />
+    </DisclosurePanel>
+</Disclosure>
