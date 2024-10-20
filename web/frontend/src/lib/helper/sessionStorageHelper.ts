d4fba934515c4aac4ef96adb46552bbad4e6e09e
Julian Rakuschek <julian.rakuschek@tugraz.at>
2024-10-20 16:16:54 +0200

    Switch to local storage

--- web/frontend/src/lib/helper/sessionStorageHelper.ts
+++ web/frontend/src/lib/helper/sessionStorageHelper.ts
@@ -2,21 +2,21 @@ import type {AnalysisPostData, Label, LabelBase} from "@lib/types";
 import { v4 as uuidv4 } from 'uuid';
 
 export const sessionGetNormals = (machine: string): string[] => {
-    const normals: { [machine: string]: string[] } = JSON.parse(sessionStorage.getItem("normals") ?? "{}")
+    const normals: { [machine: string]: string[] } = JSON.parse(localStorage.getItem("normals") ?? "{}")
     return normals[machine] ?? []
 }
 
 export const sessionToggleNormal = (machine: string, sample: string) => {
-    const normals: { [machine: string]: string[] } = JSON.parse(sessionStorage.getItem("normals") ?? "{}")
+    const normals: { [machine: string]: string[] } = JSON.parse(localStorage.getItem("normals") ?? "{}")
     let samples = normals[machine] ?? []
     if (samples.indexOf(sample) !== -1) samples = samples.filter(s => s !== sample)
     else samples.push(sample)
     normals[machine] = samples
-    sessionStorage.setItem("normals", JSON.stringify(normals))
+    localStorage.setItem("normals", JSON.stringify(normals))
 }
 
 export const sessionGetLabels = (machine: string, sample?: string): Label[] => {
-    let labels: Label[] = JSON.parse(sessionStorage.getItem("labels") ?? "[]")
+    let labels: Label[] = JSON.parse(localStorage.getItem("labels") ?? "[]")
     labels = labels.filter(l => l.machine === machine)
     if (sample) {
         labels = labels.filter(l => l.sampleId === sample)
@@ -26,21 +26,21 @@ export const sessionGetLabels = (machine: string, sample?: string): Label[] => {
 
 export const sessionAddLabel = (label: LabelBase) => {
     const label_id = uuidv4()
-    const labels: Label[] = JSON.parse(sessionStorage.getItem("labels") ?? "[]")
+    const labels: Label[] = JSON.parse(localStorage.getItem("labels") ?? "[]")
     labels.push({...label, _id: {$oid: label_id}})
-    sessionStorage.setItem("labels", JSON.stringify(labels))
+    localStorage.setItem("labels", JSON.stringify(labels))
 }
 
 export const sessionDeleteLabelPyID = (labelId: string) => {
-    let labels: Label[] = JSON.parse(sessionStorage.getItem("labels") ?? "[]")
+    let labels: Label[] = JSON.parse(localStorage.getItem("labels") ?? "[]")
     labels = labels.filter(l => l._id.$oid !== labelId)
-    sessionStorage.setItem("labels", JSON.stringify(labels))
+    localStorage.setItem("labels", JSON.stringify(labels))
 }
 
 export const sessionDeleteLabelPyPos = (machine: string, sample: string, pos: number) => {
-    let labels: Label[] = JSON.parse(sessionStorage.getItem("labels") ?? "[]")
+    let labels: Label[] = JSON.parse(localStorage.getItem("labels") ?? "[]")
     labels = labels.filter(l => l.machine !== machine || l.sampleId !== sample || !(l.from <= pos && pos <= l.to))
-    sessionStorage.setItem("labels", JSON.stringify(labels))
+    localStorage.setItem("labels", JSON.stringify(labels))
 }
 
 export const sessionGetAll = (machine: string): AnalysisPostData => {
@@ -53,12 +53,12 @@ export const sessionGetAll = (machine: string): AnalysisPostData => {
 }
 
 export const itemSeen = (machine: string, sample: string): boolean => {
-    const viewHistory: string[] = JSON.parse(sessionStorage.getItem("viewHistory") ?? "[]")
+    const viewHistory: string[] = JSON.parse(localStorage.getItem("viewHistory") ?? "[]")
     return viewHistory.indexOf(`${machine}-${sample}`) !== -1
 }
 
 export const setItemSeen = (machine: string, sample: string) => {
-    const viewHistory: string[] = JSON.parse(sessionStorage.getItem("viewHistory") ?? "[]")
+    const viewHistory: string[] = JSON.parse(localStorage.getItem("viewHistory") ?? "[]")
     viewHistory.push(`${machine}-${sample}`)
-    sessionStorage.setItem("viewHistory", JSON.stringify(viewHistory))
+    localStorage.setItem("viewHistory", JSON.stringify(viewHistory))
 }
