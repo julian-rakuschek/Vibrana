90f0c88eda2d760a49c0666f0c4abe39ecfe7547
Julian Rakuschek <julian.rakuschek@tugraz.at>
2026-04-17 16:44:30 +0200

    Temporary testing page

---
+++ vibrana/frontend/src/routes/playground/avl/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import {density1d} from 'fast-kde';
+
+    // const sample = [1,1,4,4,4,4,4,7,7];
+    const sample = [7,7];
+    const densities = density1d(sample, {bins: 10, extent: [5, 7], bandwidth: 3}).grid();
+    console.log(densities)
+</script>
