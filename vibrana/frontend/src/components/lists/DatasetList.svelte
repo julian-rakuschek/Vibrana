d81cefa37880585ce03aec620a5fd06575522a78
Julian Rakuschek <julian.rakuschek@tugraz.at>
2026-05-15 09:41:23 +0200

    Improved dataset list

--- vibrana/frontend/src/components/lists/DatasetList.svelte
+++ vibrana/frontend/src/components/lists/DatasetList.svelte
@@ -5,10 +5,12 @@
     const datasets = page.data.config
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full gap-4 p-6 justify-center items-start h-full font-[Poppins] bg-[#faf9f5]">
-    {#each Object.keys(datasets) as ds}
-        <DatasetListItem dataset={datasets[ds]} dataset_key={ds} />
-    {/each}
+<div class="w-full h-full font-[Poppins] bg-[#faf9f5]">
+    <div class="flex flex-col gap-4 w-full lg:w-1/2 mx-auto">
+        {#each Object.keys(datasets) as ds}
+            <DatasetListItem dataset={datasets[ds]} dataset_key={ds}/>
+        {/each}
+    </div>
 </div>
 
 
